e828f6ead3c5bac384f6081faf987078290ddf58
Davidjayan <davidjayandj@gmail.com>
2023-07-14 02:22:57 +0530

    [Davidjayan] Constant helpers added

---
+++ src/Utils/constants.ts
@@ -0,0 +1,68 @@
+export const benefits = [
+  {
+    title: "Benefit 1",
+    imageSrc:
+      "https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/13b0f11a-multor-benefit1-icon.svg",
+    description:
+      "Highlight the benefits of signing up for an appointment, online class,or video consultation.",
+  },
+  {
+    title: "Benefit 2",
+    imageSrc:
+      "https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/8ab92ea8-multor-benefit2-icon.svg",
+    description:
+      "For example, fitness instructors might describe how their routines make people healthier *and* happier.",
+  },
+  {
+    title: "Benefit 3",
+    imageSrc:
+      "https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/c32dd90d-multor-benefit3-icon.svg",
+    description:
+      "Remind visitors how easy it is to claim your offer and start enjoying the benefits.",
+  },
+];
+
+export const testimonies = [
+  {
+    rating: 5,
+    testimony:
+      "Share a real testimonial that hits some of your benefits (but isn't too sales-y).",
+    avatar:
+      "https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/52bdc545-multor-testimonial1-headshot_101e01e00000000000001o.jpg",
+    name: "Real Name",
+    location: "Location",
+  },
+  {
+    rating: 4.5,
+    testimony:
+      "Include someone talking about how easy it was to sign up and participate.",
+    avatar:
+      "https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/94045dbc-multor-testimonial2-headshot_101e01e00000000000001o.jpg",
+    name: "Real Name",
+    location: "Location",
+  },
+];
+
+export const features = [
+  {
+    name: "Feature 1",
+    description:
+      "Talk about some of the details of your offer with a focus on the value people get back.",
+    imageSrc:
+      "https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/53ad3716-multor-feature1-image_108c04p00000000000001o.jpg",
+  },
+  {
+    name: "Feature 2",
+    description:
+      "Is there a pain point that your service resolves? Tell visitors about it here.",
+    imageSrc:
+      "https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/adf66a57-multor-feature2-image_108c04p00000000000001o.jpg",
+  },
+  {
+    name: "Feature 3",
+    description:
+      "Alternatively, you could use this section to address some frequently asked questions.",
+    imageSrc:
+      "https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/6d486c61-multor-feature3-image_108c04p00000000000001o.jpg",
+  },
+];
